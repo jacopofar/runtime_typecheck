02ceacddcf6f3063577f8b4c94957a3957e68cca
jacopofar <jacopofar@users.noreply.github.com>
2017-08-31 22:09:08 +0200

    Add travis CI config

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ setup(
     version='0.1',
     description='Check that a value satisfies a Python 3.6 type',
     author='Jacopo Farina',
+    author_email='jacopo1.farina@gmail.com',
     license='MIT',
     packages=['runtime_typecheck'],
-    url='https://github.com/jacopofar/runtime_typecheck'
+    url='https://github.com/jacopofar/runtime_typecheck',
+    download_url='https://github.com/jacopofar/runtime_typecheck/archive/0.1.tar.gz'
 )
