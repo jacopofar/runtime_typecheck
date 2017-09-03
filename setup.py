b5ed7efaa8e47ba45255e5ae876900191ea8c503
jacopofar <jacopofar@users.noreply.github.com>
2017-09-03 19:00:25 +0200

    Minor version bump

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
 setup(
     name='runtime_typecheck',
     long_description='Runtime type checking with Python 3.6 typing functions',
-    version='0.1',
+    version='0.2',
     description='Runtime type checking with Python 3.6 typing',
     author='Jacopo Farina',
     author_email='jacopo1.farina@gmail.com',
     license='MIT',
     packages=['runtime_typecheck'],
     url='https://github.com/jacopofar/runtime_typecheck',
-    download_url='https://github.com/jacopofar/runtime_typecheck/archive/0.1.tar.gz'
+    download_url='https://github.com/jacopofar/runtime_typecheck/archive/0.2.tar.gz'
 )
